1472547e20e99fa978e362ae50a89dacd67865b2
Rumblevn <kpham7946@gmail.com>
2022-12-06 22:30:16 -0500

    finish functions game() and playGround() add console.log() for debugging purpose

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 // console.log("helloe worldddd");
 
+game();
+
 function game() {
     var humanScore = 0;
     var computerScore = 0;
     var humanHand = "";
     var computerHand = "";
+    var tempScore;
 
     for (let index = 0; index < 5; index++) {
-        humanHand = prompt("Here go...").toLowerCase;
+        // humanHand = prompt("Round " + (index+1) + " ,state your hand:").toLowerCase;
+        humanHand = "rock";
         computerHand = getComputerChoice();
-        playRound(humanHand, computerHand);
-
+        tempScore = playRound(humanHand, computerHand);
+        console.log(tempScore);
+        if (tempScore == -1) {
+            computerScore++;
+            console.log("at round " + (index+1) + " : computer win" );
+        } else if (tempScore == 1) {
+            humanScore++;
+            console.log("at round " + (index+1) + " : human win" );
+        } else {
+            console.log("at round " + (index+1) + " draw")
+        }
+        
     }
+
 
 }
 
 function playRound(playerChoice, computerChoice) {
-    if (playerChoice === ) {
-        
+    // return 0 for draw, -1 for computer win, 1 for human win
+    var score = 5;
+    console.log("playerChoice: " + playerChoice);
+    console.log("computerChoice: " + computerChoice);
+    if (playerChoice === computerChoice) {
+        score = 0;
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            score = -1;
+        } else score = 1;
+    } else if (playerChoice === "payer") {
+        if (computerChoice === "scissors") {
+            score = -1;
+        } else score = 1;
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            score = -1;
+        } else score = 1;
     }
-
+    // console.log("score: " + score);
+    return score;
 }
 
 function getComputerChoice() {
